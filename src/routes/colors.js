00aa3241f9c8bf2185d391a552f682424c0380a4
Mallikarjun Kamtam <61532466+MallikarjunKamtam@users.noreply.github.com>
2025-10-26 09:30:59 +0530

    reset

--- src/routes/colors.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const { editColors, getColors, postColors } = require('../controllers/colors')
-
-router.route('/').get(getColors).post(postColors)
-router.route('/:id').put(editColors)
-
-module.exports = router
